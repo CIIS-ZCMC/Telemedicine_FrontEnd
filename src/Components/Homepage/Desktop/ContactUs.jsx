8c0f8a956e577f1d954cad52acc2531987756dfd
Alyana Barretto <alyanabarretto.zcmc@gmail.com>
2023-06-21 14:14:52 +0800

    yana

---
+++ src/Components/Homepage/Desktop/ContactUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TitleHeader from "../TitleHeader";
+import { Box } from "@chakra-ui/react";
+
+function ContactUs(props) {
+  return (
+    <div>
+      <Box>
+        <TitleHeader title="Contact Us" />
+      </Box>
+    </div>
+  );
+}
+
+export default ContactUs;
